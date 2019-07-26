a8c3e2e9bb2bb222c72d66fe2bdc8304f0044c9c
Tu <ptvuongtu@tma.com.vn>
2019-07-26 10:31:00 +0700

    'updatecode'

--- bai41Promise/index.js
+++ bai41Promise/index.js
@@ -20,6 +20,25 @@
 //   .then(onDone)
 //   .catch(onError);
 
+var fs = require('fs');
 
+function readFilePromise(path) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(path, {encoding: 'utf8'}, function(err, data) {
+            if (err) {
+                reject(err);
+            }
+            else{
+                resolve(data);
+            }
+        });
+    });
+}
 
-
+readFilePromise('song1.txt')
+    .then(function(song1) {
+        console.log(song1);
+    })
+    .catch(function(error) {
+        console.log(error)
+    });
